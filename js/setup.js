5ceb44b73c92c13ec34d8f1ac093894e5756d672
Denis Kornienko <korney197823@gmail.com>
2017-04-09 23:02:03 +0300

    made open/close dialog window and change colors character

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var setup = document.querySelector('.setup');
+var setupOpenButton = document.querySelector('.setup-open');
+var setupCloseButton = document.querySelector('.setup-close');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 var similarListElement = setup.querySelector('.setup-similar-list');
 
@@ -9,6 +11,7 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SECONDNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
 var wizards = [
   {
@@ -52,15 +55,55 @@ for (var i = 0; i < wizards.length; i++) {
 
 similarListElement.appendChild(fragment);
 
-// объявление и вызов функции удаления класса элемента
-function removeClass(element, classRemove) {
-  element.classList.remove(classRemove);
-}
-removeClass(setup, 'hidden');
-removeClass(document.querySelector('.setup-similar'), 'hidden');
+// Функции открытия и закрытия диалога настроек персонажа
+setupOpenButton.addEventListener('click', function () {
+  setup.classList.remove('hidden');
+
+  document.addEventListener('keydown', function (event) {
+    if (event.keyCode === 27) {
+      setup.classList.add('hidden');
+    }
+  });
+});
+
+setupOpenButton.addEventListener('keydown', function (event) {
+  if (event.keyCode === 13) {
+    setup.classList.remove('hidden');
+  }
+});
+
+setupCloseButton.addEventListener('click', function () {
+  setup.classList.add('hidden');
+});
+
+setupCloseButton.addEventListener('keydown', function (event) {
+  if (event.keyCode === 13) {
+    setup.classList.add('hidden');
+  }
+});
+
+document.querySelector('.setup-similar').classList.remove('hidden');
 
 // объявление функции генерации случайных данных
 function getRandomElement(arr) {
   var random = Math.floor(Math.random() * arr.length);
   return arr[random];
 }
+
+// Функция изменения цвета мантии по нажатию
+var wizardCoat = document.querySelector('.wizard-coat');
+wizardCoat.addEventListener('click', function () {
+  wizardCoat.style.fill = getRandomElement(COAT_COLORS);
+});
+
+// Функция изменения цвета глаз по нажатию
+var wizardEyes = document.querySelector('.wizard-eyes');
+wizardEyes.addEventListener('click', function () {
+  wizardEyes.style.fill = getRandomElement(EYES_COLORS);
+});
+
+// Функция изменения цвета файрбола
+var fireball = document.querySelector('.setup-fireball-wrap');
+fireball.addEventListener('click', function () {
+  fireball.style.backgroundColor = getRandomElement(FIREBALL_COLORS);
+});
